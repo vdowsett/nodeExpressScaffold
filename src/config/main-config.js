1a53335b522ce0e39a5037e7d2c14857c52a1b92
vdowsett <vdowsett@gmail.com>
2019-02-24 09:27:23 -0600

    main-config

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+
+const keyPublishable = process.env.STRIPE_PUBLISHABLE_KEY;
+const keySecret = process.env.STRIPE_SECRET_KEY;
+
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
@@ -8,26 +12,28 @@ const flash = require("express-flash");
 const passportConfig = require("./passport-config");
 
 module.exports = {
-  init(){/* function definition */},
-  init(app, express){
+init(app, express){
+
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
-    app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(expressValidator());
-    app.use(session({
-      secret: process.env.cookieSecret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 1.21e+9 }
-    }));
-    app.use(flash());
-    passportConfig.init(app);
 
+    app.use(session({
+        secret: process.env.cookieSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 1.21e+9 }
+    }));
+
+    app.use(flash());
+
+    passportConfig.init(app);
     app.use((req,res,next) => {
-      res.locals.currentUser = req.user;
-      next();
+        res.locals.currentUser = req.user;
+        next();
     });
     
-  }
+    }
 };
