deb2e41b3cf0e8739355c5c9ed6180d7ef171c4d
vdowsett <vdowsett@gmail.com>
2019-02-24 09:06:02 -0600

    setting up controllers, views, assets

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome");
-});
+const staticController = require("../controllers/staticController");
+
+router.get("/", staticController.index);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
